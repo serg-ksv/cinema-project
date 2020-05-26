108fe4ad851b2c30d540590f70a3e765d83807ab
serg-ksv <serg.ksv1@gmail.com>
2020-05-26 11:57:40 +0300

    Added constant variable for hashing algorithm

--- src/main/java/mate/academy/cinema/util/HashUtil.java
+++ src/main/java/mate/academy/cinema/util/HashUtil.java
@@ -3,10 +3,9 @@ package mate.academy.cinema.util;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import org.apache.log4j.Logger;
 
 public class HashUtil {
-    private static final Logger LOGGER = Logger.getLogger(HashUtil.class);
+    public static final String ALGORITHM = "SHA-512";
 
     public static byte[] getSalt() {
         var random = new SecureRandom();
@@ -18,14 +17,14 @@ public class HashUtil {
     public static String hashPassword(String password, byte[] salt) {
         var hashedPassword = new StringBuilder();
         try {
-            var messageDigest = MessageDigest.getInstance("SHA-512");
+            var messageDigest = MessageDigest.getInstance(ALGORITHM);
             messageDigest.update(salt);
             byte[] digest = messageDigest.digest(password.getBytes());
             for (byte b : digest) {
                 hashedPassword.append(String.format("%02x", b));
             }
         } catch (NoSuchAlgorithmException e) {
-            LOGGER.error(e);
+            throw new RuntimeException("Unsuccessful password hashing.", e);
         }
         return hashedPassword.toString();
     }
