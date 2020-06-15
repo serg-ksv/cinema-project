8fb9b2d7ab68872db2450ef848ab801a774fbeb5
serg-ksv <serg.ksv1@gmail.com>
2020-06-15 18:42:36 +0300

    Removed redundant parameter

--- src/main/java/mate/academy/cinema/security/EmailConstraint.java
+++ src/main/java/mate/academy/cinema/security/EmailConstraint.java
@@ -8,7 +8,7 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 
 @Constraint(validatedBy = EmailValidator.class)
-@Target({ ElementType.FIELD, ElementType.METHOD })
+@Target({ ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EmailConstraint {
     String message() default "Invalid email format";
