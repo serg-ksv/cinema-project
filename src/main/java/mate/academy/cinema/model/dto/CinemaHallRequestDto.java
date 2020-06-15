f573349ab2f0e606b46b5d4f1b14144c36ecd404
serg-ksv <serg.ksv1@gmail.com>
2020-06-15 18:41:02 +0300

    Removed NotNull annotation

--- src/main/java/mate/academy/cinema/model/dto/CinemaHallRequestDto.java
+++ src/main/java/mate/academy/cinema/model/dto/CinemaHallRequestDto.java
@@ -3,10 +3,8 @@ package mate.academy.cinema.model.dto;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 public class CinemaHallRequestDto {
-    @NotNull(message = "Capacity can't be null!")
     @Min(50)
     @Max(400)
     private int capacity;
